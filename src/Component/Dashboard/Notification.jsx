00b9cae55a3fa84f52be4250f895fa9e7c3a3fdc
tanu1997 <tanupriya.srivastava@nbgspl.com>
2022-08-24 16:23:56 +0530

    shreya

--- src/Component/Dashboard/Notification.jsx
+++ src/Component/Dashboard/Notification.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Notification() {
     return (
         <div className='wrapper'>
-           <h1>Notification</h1> 
+           <h1>Notification </h1> 
         </div>
     )
 }
